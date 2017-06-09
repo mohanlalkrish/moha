223fd617779d7e585fd89de3c56fd8e56426d8f7
mohanlalkrish <mohanlal051997@gmail.com>
2017-06-09 01:15:21 -0700

    Create conv.java

---
+++ conv.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class conv
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String number = s.nextLine();
+int result = Integer.parseInt(number);
+System.out.println(result);
+}
+}
