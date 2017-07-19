9d94a7b54e89819757b089901fd240210a663457
mohanlalkrish <mohanlal051997@gmail.com>
2017-07-19 14:21:23 +0530

    Create new file

---
+++ arr.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class arr
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<args.length;i++)
+        {
+            int n=Integer.parseInt(args[i]);
+        if(i==n)
+        {
+            System.out.print(n+" ");
+        }
+        else
+        {
+            System.out.print("0");
+        }
+        }
+    }
+}
+
