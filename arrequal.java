a6c071826dc3e997a011371ef5c09d8f677f28b0
mohanlalkrish <mohanlal051997@gmail.com>
2017-07-20 15:42:04 +0530

    Update arrequal.java

--- arrequal.java
+++ arrequal.java
@@ -4,17 +4,15 @@ class arrequal {
 	public static void main(String[]args){
 		Scanner in=new Scanner(System.in);
 		int n;
-		System.out.println("Enter the no.of elements");
 		n=in.nextInt();
 		int a[]=new int[n];
 		for(int i=0;i<n;i++){
-			System.out.println("enter the elements");
 			a[i]=in.nextInt();
 		}
 		for(int i=0;i<n;i++){
 			for(int j=i+1;j<n;j++){
 				if(a[i]==a[j]){
-					System.out.println(a[i]);
+					System.out.print(a[i]);
 				}}}
 		
 	}
